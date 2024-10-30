f6e0fc921a1521c53e4b7553d71b15dd67020f40
siddharthsai20 <113754786+siddharthsai20@users.noreply.github.com>
2024-10-30 15:59:14 -0500

    updates users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+let value = 0;
+let total = 1;
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  value += 2;       
+  total += value * 2; 
+  res.send(Total is: ${total});  
 });
 
 module.exports = router;
