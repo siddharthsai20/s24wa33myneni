87c4c666e2400c361db4b5a2f356c965e3783ba5
siddharthsai20 <113754786+siddharthsai20@users.noreply.github.com>
2024-10-30 15:42:30 -0500

    updated code

--- app.js
+++ app.js
@@ -4,36 +4,41 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Import routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var mydataRouter = require('./routes/mydata');  // New route
+var mydataRouter = require('./routes/mydata');  // New route for /mydata
 
 var app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Define routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/mydata', mydataRouter);  // Registering the new route
+app.use('/mydata', mydataRouter);  // Register /mydata route
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET mydata page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Your Full Name' }); // Replace 'Your Full Name' with your actual name
+  res.render('mydata', { title: 'sidhu' }); 
 });
 
 module.exports = router;
